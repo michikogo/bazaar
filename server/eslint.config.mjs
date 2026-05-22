22e287d4c0d38380b5cf2fe22873b7c85b331a57
Michiko Go <michikogo98@gmail.com>
2026-05-22 16:34:45 -0400

    chore: add ESLint and Prettier config for client and server

---
+++ server/eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js"
+import prettier from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
+
+export default [
+  js.configs.recommended,
+  prettierConfig,
+  {
+    plugins: { prettier },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+]
